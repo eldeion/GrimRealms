48ea759012e2c25be86f8c1c878aa8b3e73f1755
eldeion <vncntdmrs@gmail.com>
2018-03-28 21:31:32 -0400

    Note Script

---
+++ GrimRealms/Assets/Models/Objects/Note/Materials/Note.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Note : MonoBehaviour {
+
+	public Image noteImage;
+	public AudioClip pickupSound;
+	public AudioClip putAwaySound;
+	// Use this for initialization
+	void Start () 
+	{
+		noteImage.enabled = false;
+	}
+
+	public void ShowNoteImage()
+	{
+		noteImage.enabled = true;
+		GetComponent<AudioSource> ().PlayOneShot (pickupSound);
+	}
+
+	public void HideNoteImage()
+	{
+		GetComponent<AudioSource> ().PlayOneShot (putAwaySound);
+	}
+}
